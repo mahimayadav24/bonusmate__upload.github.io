407a0d0d41e8dd93f230a4e22124b6cf31bbe147
mahimayadav24 <88049838+mahimayadav24@users.noreply.github.com>
2021-09-27 14:33:44 +0530

    JS

---
+++ JS/TT2.js
@@ -0,0 +1,374 @@
+$("#ToolTip2").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip3").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip4").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip5").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip6").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip7").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// ####
+$("#ToolTip8").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip9").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip10").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// #####
+$("#ToolTip11").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip12").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip13").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+
+// ####
+$("#ToolTip14").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip15").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip16").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// ######
+$("#ToolTip17").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip18").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip19").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+
+// #####
+$("#ToolTip20").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip21").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip22").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// #####
+$("#ToolTip23").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip24").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip25").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// #####
+$("#ToolTip26").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip27").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip28").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// #####
+$("#ToolTip29").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip30").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip31").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// #####
+$("#ToolTip32").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip33").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip34").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// #####
+$("#ToolTip35").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip36").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip37").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// #####
+$("#ToolTip38").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip39").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip40").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+// #####
+
+$("#ToolTip41").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip42").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip43").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip44").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "View Page",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip45").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Edit in Builder",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
+
+$("#ToolTip46").easyTooltip({
+    tooltipPosition: 'relative',
+    content: "Delete",
+    tooltipArwBorderWidth: 12,
+    tooltipBgColor: "rgba(0,0,0,1)",
+    tooltipFtColor: "#fff",
+});
